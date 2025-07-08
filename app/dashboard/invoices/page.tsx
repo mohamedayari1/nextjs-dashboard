384697f4fd140eabb9a64cfc3b8ddf3f181fc266
mohamedayari1 <ayarim781@gmail.com>
2025-07-08 20:57:44 +0100

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Page() {
+    return <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+       invoices page
+    </p>;
+}
